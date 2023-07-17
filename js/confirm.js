0a2e3df714f16dbfbe0d365c1559407db42faaeb
MenthicalTech <101377846+MenthicalTech@users.noreply.github.com>
2023-07-17 07:25:14 +0300

    Add files via upload

--- js/confirm.js
+++ js/confirm.js
@@ -5,8 +5,11 @@ document.addEventListener("DOMContentLoaded", function() {
   var email = getCookie("email");
   var phoneNumber = getCookie("phoneNumber");
   var signUpNewsletter = getCookie("signUpNewsletter");
+  var interests = getCookie("interests");
+  var comments = getCookie("comments");
+  var referredBy = getCookie("referredBy");
 
-  displayData(userName, firstName, lastName, email, phoneNumber, signUpNewsletter);
+  displayData(userName, firstName, lastName, email, phoneNumber, signUpNewsletter, interests, comments, referredBy);
 
   function getCookie(name) {
     var value = "; " + document.cookie;
@@ -14,13 +17,25 @@ document.addEventListener("DOMContentLoaded", function() {
     if (parts.length === 2) return parts.pop().split(";").shift();
   }
 
-
-  function displayData(userName, firstName, lastName, email, phoneNumber, signUpNewsletter) {
+  function displayData(userName, firstName, lastName, email, phoneNumber, signUpNewsletter, interests, comments, referredBy) {
     document.getElementById("userName").textContent = userName;
     document.getElementById("firstName").textContent = firstName;
     document.getElementById("lastName").textContent = lastName;
     document.getElementById("email").textContent = email;
     document.getElementById("phoneNumber").textContent = phoneNumber;
     document.getElementById("signUpNewsletter").textContent = signUpNewsletter;
+    document.getElementById("comments").textContent = comments;
+    document.getElementById("referredBy").textContent = referredBy;
+
+    // Parse the interests JSON data back into an array
+    var interestsArray = JSON.parse(interests);
+
+    // Display interests as a list
+    var interestsList = document.getElementById("interests");
+    interestsArray.forEach(function(interest) {
+      var listItem = document.createElement("li");
+      listItem.textContent = interest;
+      interestsList.appendChild(listItem);
+    });
   }
 });
--- js/interests.js
+++ js/interests.js
@@ -3,9 +3,68 @@ document.addEventListener("DOMContentLoaded", function() {
 
   form.addEventListener("submit", function(event) {
     event.preventDefault();
-    saveDataToCookies();
-    window.location.href = "confirm.html";
+    validateForm();
   });
+
+  function validateForm() {
+    var interests = form.querySelectorAll("input[name='interests']:checked");
+    // var signUpNewsletter = form.querySelector("input[name='signUpNewsletter']:checked");
+
+    var isValid = true;
+
+    // Clear previous error messages
+    clearErrorMessages();
+
+    // Validate interests
+    if (interests.length === 0) {
+      displayErrorMessage(form.querySelector("label[for='interests']"), "Please select at least one interest.");
+      isValid = false;
+    }
+
+    // Validate newsletter sign up
+    // if (!signUpNewsletter) {
+    //   displayErrorMessage(form.querySelector("label[for='signUpNewsletter']"), "Please select your newsletter sign-up preference.");
+    //   isValid = false;
+    // }
+
+    // Set focus to the first erroneous input field
+    if (!isValid) {
+      setFocusToFirstError();
+    }
+
+    // If the form is valid, store data in cookies and submit it
+    if (isValid) {
+      saveDataToCookies();
+      form.submit();
+      // Clear the form after submission
+      clearFormFields();
+    }
+  }
+
+  function displayErrorMessage(input, message) {
+    var error = document.createElement("p");
+    error.className = "error-message";
+    error.textContent = message;
+    input.parentNode.insertBefore(error, input.nextSibling);
+  }
+
+  function clearErrorMessages() {
+    var errorMessages = document.querySelectorAll(".error-message");
+    errorMessages.forEach(function(errorMessage) {
+      errorMessage.parentNode.removeChild(errorMessage);
+    });
+  }
+
+  function setFocusToFirstError() {
+    var firstError = document.querySelector(".error-message");
+    if (firstError) {
+      firstError.previousSibling.focus();
+    }
+  }
+
+  function clearFormFields() {
+    form.reset();
+  }
 
   function saveDataToCookies() {
     var formData = new FormData(form);
@@ -14,11 +73,40 @@ document.addEventListener("DOMContentLoaded", function() {
       return cookie.split("=")[0];
     });
 
+    // Set default value to "None" for comments and referredBy if they are blank
+    if (!formData.get("comments")) {
+      formData.set("comments", "None");
+    }
+
+    if (!formData.get("referredBy")) {
+      formData.set("referredBy", "None");
+    }
+
+    // Remove previously stored interests data from cookies
+    cookieKeys.forEach(function(cookieKey) {
+      if (cookieKey.startsWith("interests")) {
+        document.cookie = cookieKey + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+      }
+    });
+
+    // Save new interests data to cookies as a list
+    var interests = [];
+    formData.getAll("interests").forEach(function(interest) {
+      interests.push(interest);
+    });
+
+    // Save other form data to cookies
     formData.forEach(function(value, name) {
       // Check if the field is not already stored in cookies
       if (!cookieKeys.includes(name)) {
         document.cookie = `${name}=${value}`;
       }
     });
+
+    // Save interests data in a single cookie with name "interests"
+    document.cookie = `interests=${JSON.stringify(interests)}`;
+    document.cookie = `comments=${formData.get("comments")}`;
+    document.cookie = `referredBy=${formData.get("referredBy")}`;
   }
 });
+  
